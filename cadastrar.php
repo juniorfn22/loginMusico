1cc403a550876552776003faf04a00801b65801e
Junior Ferreira <juniorfn22@gmail.com>
2019-06-30 22:50:07 -0300

    jr

---
+++ cadastrar.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include('conexao.php');
+
+$nome  = mysqli_real_escape_string($conexao, trim($_POST['nome']));
+$login = mysqli_real_escape_string($conexao, trim($_POST['usuario']));
+$senha = mysqli_real_escape_string($conexao, trim(md5($_POST['senha'])));
+$naipe = mysqli_real_escape_string($conexao, trim($_POST['naipe']));
+
+$sql    = "select count(*) as total from usuario where usuario = '$usuario'";
+$result = mysqli_query($conexao, $sql);
+$row    = mysqli_fetch_assoc($result);
+
+if ($row['total'] == 1){
+    $_SESSION['usuario_existe'] = true;
+    header('Location:cadastro.php');
+    exit();
+}
+
+$sql = "insert into musico (nome,login,senha,naipe) values ('$nome','$login','$senha','$naipe')";
+
+if ($conexao->query($sql) === true){
+    $_SESSION['status_cadastro'] = true;
+}
+
+$conexao->close();
+header('Location:cadastro.php');
+exit();
+
+?>
---
+++ cadastro.php
@@ -0,0 +1,96 @@
+<?php 
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+    
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Cadastro</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+
+<body>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Sistema de Cadastro</h3>
+                    <?php 
+                    if (isset($_SESSION['status_cadastro'])){
+                    ?>
+                    <div class="notification is-success">
+                      <p>Cadastro efetuado!</p>
+                      <p>Faça login informando o seu usuário e senha <a href="login.php">aqui</a></p>
+                    </div>
+                    <?php 
+                    };
+                    unset($_SESSION['status_cadastro']);
+                    ?>
+
+                    <?php 
+                    if (isset($_SESSION['usuario_existe'])){
+                    ?>
+                    <div class="notification is-info">
+                        <p>O usuário escolhido já existe. Informe outro e tente novamente.</p>
+                    </div>
+                    <?php 
+                    };
+                    unset($_SESSION['usuario_existe']);
+                    ?>
+                    <div class="box">
+                        <form action="cadastrar.php" method="POST">
+                            <div class="field">
+                                <div class="control">
+                                    <input name="nome" type="text" class="input is-large" style="color:black;" placeholder="Nome" autofocus>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input name="usuario" type="text" class="input is-large" style="color:black;" placeholder="Usuário">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input name="senha" class="input is-large" style="color:black;"  type="password" placeholder="Senha">
+                                </div>
+                            </div>
+                            <div class="fiel">
+                                <div class="control">
+                                    <select name="naipe" id="naipe" class="input is-large" >
+                                        <option value="" class="op" id="op" disable selected>selecione o naipe</option>
+                                        <option value="0">Maestro</option>
+                                        <option value="1">1º Clarinete</option>
+                                        <option value="2">2º Clarinete</option>
+                                        <option value="3">1º Trompete</option>
+                                        <option value="4">2º Trompete</option>
+                                        <option value="5">Flauta</option>
+                                        <option value="6">Sax Alto</option>
+                                        <option value="7">Sax Tenor</option>
+                                        <option value="8">Trombone</option>
+                                        <option value="9">Tuba Bb</option>
+                                        <option value="10">Tuba Eb</option>
+                                        <option value="11">Violino</option>
+                                    </select>
+                                </div>
+                            </div>
+                                <br>
+                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Cadastrar</button>
+                        </form>
+                        <div class="field">
+                                <a href="index.php">Voltar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
