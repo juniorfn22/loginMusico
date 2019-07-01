61d52477cf026b9241982ff55302f302eb26a5f1
Junior Ferreira <juniorfn22@gmail.com>
2019-06-30 23:36:51 -0300

    windows 2

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ if (empty($_POST['usuario']) || empty($_POST['senha'])){
     exit();
 }
 
-
+#comentário
 $login = mysqli_real_escape_string ($conexao, $_POST['usuario']);
 $senha = mysqli_real_escape_string ($conexao, $_POST['senha']);
 
